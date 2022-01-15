c509f7b7365e7ffbf3c1d7c206ddd36ff04e1ac2
Rupali0311 <rupalitiwary0311@gmail.com>
2022-01-15 22:32:15 +0530

    App complete

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context)!.settings.arguments as Map;
+    data = data.isNotEmpty ? data : ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
     //set background
@@ -35,8 +35,16 @@ class _HomeState extends State<Home> {
               child: Column(
                 children:[
                   FlatButton.icon(
-                    onPressed:(){
-                      Navigator.pushNamed(context, '/location');
+                    onPressed:() async{
+                      dynamic result = await Navigator.pushNamed(context, '/location');
+                      setState((){
+                        data = {
+                          'time':result['time'],
+                          'location':result['location'],
+                          'isDaytime':result['isDaytime'],
+                          'flag':result['flag']
+                        };
+                      });
                     },
                     icon:Icon(
                         Icons.edit_location,
